1ca57e042bb4c804cc4fbac4b10c86cada9aa108
Victor Magalhães Pacheco <victor.magalhaesp@gmail.com>
2017-07-11 11:26:41 -0300

    first commit

---
+++ client/app/main.ts
@@ -0,0 +1,5 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module'; // importa o módulo que será carregado primeiro
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
