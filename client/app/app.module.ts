e22c1f10a09cb12d721f33784cd5eb175f920a6c
Victor Magalhães Pacheco <victor.magalhaesp@gmail.com>
2017-07-12 11:30:56 -0300

    Adicionando validação

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -10,9 +10,10 @@ import { CadastroComponent  } from './cadastro/cadastro.component';
 import { ListagemComponent  } from './listagem/listagem.component';
 
 import { routing } from './app.routes';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports:      [ BrowserModule, FotoModule, HttpModule, PainelModule, routing ],
+  imports:      [ BrowserModule, FotoModule, HttpModule, PainelModule, routing, FormsModule, ReactiveFormsModule ],
   declarations: [ AppComponent, CadastroComponent, ListagemComponent],
   bootstrap:    [ AppComponent ]
 })
--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input } from '@angular/core';
+import { FotoComponent } from '../foto/foto.component';
+import { Http, Headers } from '@angular/http';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
     moduleId: module.id,
@@ -6,5 +10,30 @@ import { Component, Input } from '@angular/core';
     'templateUrl': './cadastro.component.html'
 })
 export class CadastroComponent {
+    foto: FotoComponent = new FotoComponent();
+    http: Http;
+    meuForm: FormGroup;
 
+    constructor(http: Http, fb: FormBuilder) {
+        this.http = http;
+        this.meuForm = fb.group({
+            titulo: ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+            url: ['', Validators.required],
+            descricao: ['', ]
+        }); 
+    }
+
+    cadastrar(event) {
+        event.preventDefault();
+
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+
+        this.http.post('v1/fotos', JSON.stringify(this.foto), {headers: headers})
+        .subscribe(() => {
+            this.foto = new FotoComponent();
+            console.log('Foto salva com sucesso');
+        }, erro => console.log(erro));
+    }
 }
