34eb2fffc07c426f6622bf3f5c73ba8a7727e437
Victor Magalhães Pacheco <victor.magalhaesp@gmail.com>
2017-07-12 14:14:09 -0300

    Enviando componente css pelo angular

--- client/app/painel/painel.component.ts
+++ client/app/painel/painel.component.ts
@@ -3,7 +3,8 @@ import { Component, Input } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'painel',
-    templateUrl: './painel.component.html'
+    templateUrl: './painel.component.html',
+    styleUrls: ['./painel.component.css']
 })
 export class PainelComponent {
     @Input() titulo: string;
